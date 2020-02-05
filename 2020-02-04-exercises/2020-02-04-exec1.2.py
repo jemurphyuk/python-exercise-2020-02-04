3cf0f2771299bb4980592f8f9fac2041f9773c9c
James Murphy <jmurphy@spartaglobal.com>
2020-02-05 10:47:30 +0000

    edited exericse 1.2

--- 2020-02-04-exercises/2020-02-04-exec1.2.py
+++ 2020-02-04-exercises/2020-02-04-exec1.2.py
@@ -1,9 +1,9 @@
 
-name = input("Hello, what is your first name? :    ").capitalize()
+name = input("Hello, what is your first name? :    ").capitalize().strip()
 eye_colour = input(f"{name} huh, what a lovely name! What colour are your eyes? :    ").lower()
 hair_colour = input("Beautiful! What about the colour of your hair? :    ").lower()
-age = int(input (f"Gorgeous! And finally {name}, how old are you? :    "))
+age = int(input (f"Gorgeous! And finally {name}, how old are you? :    ").strip())
 
 
 print (f"Hello {name}, you have {eye_colour} eyes and {hair_colour} hair, and you are {age} years old. Have a nice day! :)")
-print (f"Also {name}, I predict you were born in {2020 - age} or {2020 - 1 - age}, as you said you were {age} years old!")
+print (f"Also {name}, I predict you were born in {2020 - age} or {2019 - age}, as you said you were {age} years old!")
